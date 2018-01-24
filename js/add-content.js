dc73054b2d40ccda1bbe075ab88ee46263aa83c6
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-01-24 14:48:11 +0000

    ver3
    added days of week

--- js/add-content.js
+++ js/add-content.js
@@ -2,14 +2,25 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
+var day = today.getDay();
+var weekDay;
+
+if (day == 3){
+	weekDay = "Wednesday";
+} else if (day ==4 ) {
+	weekDay = "Thursday";
+} else if (day == 5) {
+	weekDay = "Friday";
+}
+
 if (hourNow > 18) {
-	greeting = 'Good evening!';
+	greeting = 'Good evening, its ' + weekDay;
 } else if (hourNow > 12) {
-	greeting = 'Good afternoon!';
+	greeting = 'Good afternoon Conor! Happy ' + weekDay;
 } else if (hourNow > 0) {
-	greeting = 'Good morning!';
+	greeting = 'Good morning Conor! Happy ' + weekDay;
 } else {
 	greeting = 'Welcome!';
 }
 
-document.write('<h3>' + greeting + '</h3');
+document.write('<h3>' + greeting + '</h3>');
